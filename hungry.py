0268ff4d8e8b2359d7e0c75079266307cb3aa5de
Sumit <sumitsaurav121@gmail.com>
2019-08-03 10:10:13 +0530

    new changes in hungry.py

--- hungry.py
+++ hungry.py
@@ -3,13 +3,7 @@ hungry = input('are you hungry')
 if hungry=='yes':
     print('Eat Samosa')
     print('Eat Pizza')
-    print('Eat Burger')
-    print('Eat Fries')
+    print('Eat Dosa')
 else:
-    thirsty = input('are you thirsty')
-    if thirsty == 'yes':
-        print('drink water')
-        print('drink soda')
-    else:
-        print('Do your homework')
+    print('Do your homework')
         
